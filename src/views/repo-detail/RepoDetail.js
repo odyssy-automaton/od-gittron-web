628d7f9232d8ddbd8c2f43ea2e2afa7e870ad9fa
skuhlmann <samkuhlmann@gmail.com>
2019-02-13 11:29:43 -0700

    adds lists to repo page

--- src/views/repo-detail/RepoDetail.js
+++ src/views/repo-detail/RepoDetail.js
@@ -1,28 +1,54 @@
 import React, { Component, Fragment } from 'react';
 import { Web3Consumer } from 'web3-react';
-// import { get } from '../../util/requests';
+import { get } from '../../util/requests';
+import BotList from '../../components/shared/bot-list/BotList';
+
+import './RepoDetail.scss';
 
 class RepoDetail extends Component {
   state = {
-    repo: {
-      repo: 'crockswap',
-    },
+    bots: [],
+    masterBot: {},
+    supporters: [],
+    workers: [],
   };
 
-  // componentDidMount = async () => {
-  //   // const { data } = await get(`tokenid/${this.props.match.params.tokenId}`);
-  //   // this.setState({
-  //   //   bot: data,
-  //   // });
-  // };
+  componentDidMount = async () => {
+    const { data } = await get(`tokens/repo/${this.props.match.params.ghid}`);
+    this.setState({
+      bots: data,
+      masterBot: data.find((bot) => bot.tokenType === 'master'),
+      supporters: data.filter((bot) => bot.tokenType === 'supporter'),
+      workers: data.filter((bot) => bot.tokenType === 'worker'),
+    });
+  };
 
   render() {
-    const { repo } = this.state;
+    const { bots, masterBot, supporters, workers } = this.state;
     return (
       <Web3Consumer>
         {(context) => (
           <Fragment>
-            <h2>{repo.repo}</h2>
+            {
+              <div>
+                <h2>{masterBot.repo}</h2>
+                <h3>{bots.length} total bots</h3>
+                <div className="RepoDetail__Lists">
+                  <div>
+                    <h4>Master bot</h4>
+                    <BotList bots={[masterBot]} />
+                  </div>
+                  <div>
+                    <h4>Support bots</h4>
+                    <BotList bots={supporters} />
+                  </div>
+                  <div>
+                    <h4>Worker bots</h4>
+                    <BotList bots={workers} />
+                  </div>
+                </div>
+              </div>
+            }
           </Fragment>
         )}
       </Web3Consumer>
