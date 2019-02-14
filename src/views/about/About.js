c83135ca73db7a2b6adf0c54a287ce0ac2d85f6f
oovg <office@vengist.com>
2019-02-14 12:17:58 -0700

    more about content

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -5,10 +5,10 @@ function About() {
     <div className="Page">
       <div className="PageHeader">
         <div className="PageHeader__Contents">
-          <h1>The year is 3369 and, throughout the universe, all biological life has been decimated. It's up to the Bots to build the future they want.</h1>
+          <h1>The year is 3369 and, throughout the universe, all biological life has been decimated. It's up to the Prime Bots to buidl their own future. They'll need help from the Worker and Support Bots in order to survive.</h1>
         </div>
       </div>
-      <div className="Columns">
+      <div className="Columns Contain">
         <div className="Columns__Column--33">
           <h2>Prime Bots</h2>
           <p>If you manage or own a repo, you can generate a unique Prime Bot.</p>
@@ -19,7 +19,7 @@ function About() {
         </div>
         <div className="Columns__Column--33">
           <h2>Support Bots</h2>
-          <p>Non-coders or just folks who want to support an open source project, can clone a Prime Bot as a Support Bot, generating a unique version that repo's Prime Bot. Funds go directly to the owner of the repo.</p>
+          <p>Non-coders or just folks who want to support an open source project, can clone a Prime Bot as a Support Bot, generating a unique version of that repo's Prime Bot. Funds go directly to the owner of the repo.</p>
         </div>
       </div>
       <div className="Block">
