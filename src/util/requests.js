e18169b18831beb1bf485981c49ab76616bf7343
dekan <dekanbro@gmail.com>
2019-02-16 14:35:08 -0700

    add api and network check

--- src/util/requests.js
+++ src/util/requests.js
@@ -1,16 +1,31 @@
 import axios from 'axios';
+import Web3 from 'web3';
 
-const baseURL = process.env.REACT_APP_API_HOST;
+export const BaseUrl = async () => {
+  const web3 = new Web3(Web3.givenProvider);
+  if (!web3) {
+    console.log('none');
+    return process.env.REACT_APP_MAIN_API_HOST;
+  }
+  const network = await web3.eth.net.getId();
+  console.log('network', network);
 
-console.log('process.env.REACT_APP_API_HOST');
-console.log(process.env.REACT_APP_API_HOST);
-
-export const instance = axios.create({
-  baseURL,
-  headers: { 'Content-Type': 'application/json' },
-});
+  if (network === 1) {
+    console.log('main');
+    return process.env.REACT_APP_MAIN_API_HOST;
+  } else {
+    console.log('rinkeby');
+    return process.env.REACT_APP_API_HOST;
+  }
+};
 
 export const get = async (endpoint) => {
+  const baseURL = await BaseUrl();
+
+  const instance = axios.create({
+    baseURL,
+    headers: { 'Content-Type': 'application/json' },
+  });
   try {
     return await instance.get(`/${endpoint}`);
   } catch (err) {
@@ -19,6 +34,12 @@ export const get = async (endpoint) => {
 };
 
 export const post = async (endpoint, payload) => {
+  const baseURL = await BaseUrl();
+
+  const instance = axios.create({
+    baseURL,
+    headers: { 'Content-Type': 'application/json' },
+  });
   try {
     return await instance.post(`/${endpoint}`, payload);
   } catch (err) {
@@ -27,6 +48,12 @@ export const post = async (endpoint, payload) => {
 };
 
 export const put = async (endpoint, payload) => {
+  const baseURL = await BaseUrl();
+
+  const instance = axios.create({
+    baseURL,
+    headers: { 'Content-Type': 'application/json' },
+  });
   try {
     return await instance.put(`/${endpoint}`, payload);
   } catch (err) {
@@ -35,6 +62,11 @@ export const put = async (endpoint, payload) => {
 };
 
 export const patch = async (endpoint, payload) => {
+  const baseURL = await BaseUrl();
+  const instance = axios.create({
+    baseURL,
+    headers: { 'Content-Type': 'application/json' },
+  });
   try {
     return await instance.patch(`/${endpoint}`, payload);
   } catch (err) {
