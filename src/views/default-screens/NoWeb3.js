d59185a2c4fe75c15d8686a31b0acbd48b95319c
dekan <dekanbro@gmail.com>
2019-02-17 12:12:18 -0700

    add link to mm

--- src/views/default-screens/NoWeb3.js
+++ src/views/default-screens/NoWeb3.js
@@ -5,7 +5,6 @@ import { Helmet } from 'react-helmet';
 import Routes from '../../Routes';
 import Header from '../../components/shared/header/Header';
 
-
 class NoWeb3 extends Component {
   render() {
     return (
@@ -16,6 +15,17 @@ class NoWeb3 extends Component {
         <BrowserRouter>
           <Fragment>
             <Header authenticated={false} />
+            <p>
+              *You need a web3 enabled browser to participate. Get the Chrome{' '}
+              <a
+                href="https://metamask.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                MetaMask
+              </a>{' '}
+              addon or use Brave.
+            </p>
             <Routes />
           </Fragment>
         </BrowserRouter>
