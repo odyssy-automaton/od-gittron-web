80e643a9f69cd64cdd5c6c226d4a4a2f013454fa
dekan <dekanbro@gmail.com>
2019-02-25 15:50:56 -0700

    erfactor web3 setup

--- src/App.js
+++ src/App.js
@@ -2,41 +2,40 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
-import Web3Provider from 'web3-react';
-import { AuthProvider } from './contexts/AuthContext';
+import Web3Provider, { Connectors } from 'web3-react';
 
 import Routes from './Routes';
 import Header from './components/shared/header/Header';
 
-import screens from './views/default-screens';
+// import screens from './views/default-screens';
 
 import './App.scss';
 
-class App extends Component {
-  networks = [1, 4];
-  authValue = {
-    web3enabled: true,
-  };
+const { MetaMaskConnector, NetworkOnlyConnector } = Connectors;
+const MetaMask = new MetaMaskConnector({ supportedNetworks: [1, 4] });
+const Infura = new NetworkOnlyConnector({
+  providerURL: process.env.REACT_APP_MAIN_REMOTE_WEB3_PROVIDER,
+});
+const connectors = { MetaMask, Infura };
 
-  render() {
-    return (
-      <div className="App">
-        <Helmet>
-          <meta name="description" content="gittron" />
-        </Helmet>
-        <Web3Provider screens={screens} supportedNetworks={this.networks}>
-          <BrowserRouter>
-            <Fragment>
-              <AuthProvider value={this.authValue}>
-                <Header />
-                <Routes />
-              </AuthProvider>
-            </Fragment>
-          </BrowserRouter>
-        </Web3Provider>
-      </div>
-    );
-  }
+console.log(MetaMask);
+
+function App() {
+  return (
+    <div className="App">
+      <Helmet>
+        <meta name="description" content="gittron" />
+      </Helmet>
+      <Web3Provider connectors={connectors} libraryName="web3.js">
+        <BrowserRouter>
+          <Fragment>
+            <Header />
+            <Routes />
+          </Fragment>
+        </BrowserRouter>
+      </Web3Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/shared/header/HeaderLinks.js
+++ src/components/shared/header/HeaderLinks.js
@@ -1,105 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useWeb3Context } from 'web3-react';
 
 import './Header.scss';
 import './Navbar.scss';
 import './Hamburger.scss';
+import HamburgerNav from './HamburgerNav';
 
-const HeaderLinks = class extends React.Component {
-  state = {
-    navOpen: false,
-  };
+function HeaderLinks() {
+  const context = useWeb3Context();
 
-  toggleNav = () => {
-    this.setState({
-      navOpen: !this.state.navOpen,
-    });
-  };
-
-  render() {
-    const { authenticated, networkId } = this.props;
-    const mobileNavClass = this.state.navOpen
-      ? 'Navbar__Mobile Navbar__Mobile--Open'
-      : 'Navbar__Mobile';
-    const hamburgerClass = this.state.navOpen
-      ? 'navbar__hamburger hamburger hamburger--spin is-active'
-      : 'navbar__hamburger hamburger hamburger--spin';
-
-    return (
-      <div className="Header">
-        <div className="Logo">
-          <h1 className="Header__title">
-            <Link to="/">Gittron</Link>
-          </h1>
-        </div>
-        <div className="Navbar__Desktop">
-          <Link to="/" className="Navbar__Desktop--Item">
-            Bots
-          </Link>
-          <Link to="/about" className="Navbar__Desktop--Item">
-            About
-          </Link>
-          {authenticated ? (
-            <Link to="/dashboard" className="Navbar__Desktop--Item">
-              Dashboard
-            </Link>
-          ) : null}
-          {networkId === 4 ? (
-            <p className="Rinkeby Navbar__Desktop--Item">Rinkeby</p>
-          ) : (
-            <p className="Mainnet Navbar__Desktop--Item">Main</p>
-          )}
-        </div>
-        {/* Start Hamburger */}
-        <button
-          className={hamburgerClass}
-          type="button"
-          onClick={this.toggleNav}
-        >
-          <span className="hamburger-box">
-            <span className="hamburger-inner" />
-          </span>
-        </button>
-        <div className={mobileNavClass}>
-          <div className="Navbar__Mobile--Contents">
-            <Link
-              onClick={this.toggleNav}
-              className="Navbar__Mobile--Contents--Item"
-              to="/"
-            >
-              Bots
-            </Link>
-            {authenticated ? (
-              <Link
-                onClick={this.toggleNav}
-                className="Navbar__Mobile--Contents--Item"
-                to="/dashboard"
-              >
-                Dashboard
-              </Link>
-            ) : null}
-            {authenticated ? (
-              <Link
-                onClick={this.toggleNav}
-                className="Navbar__Mobile--Contents--Item"
-                to="/repos"
-              >
-                Repos
-              </Link>
-            ) : null}
-            <Link
-              onClick={this.toggleNav}
-              className="Navbar__Mobile--Contents--Item"
-              to="/about"
-            >
-              About
-            </Link>
-          </div>
-        </div>
-        {/* End Hamburger */}
+  return (
+    <div className="Header">
+      <div className="Logo">
+        <h1 className="Header__title">
+          <Link to="/">Gittron</Link>
+        </h1>
       </div>
-    );
-  }
-};
+      <div className="Navbar__Desktop">
+        <Link to="/" className="Navbar__Desktop--Item">
+          Bots
+        </Link>
+        <Link to="/about" className="Navbar__Desktop--Item">
+          About
+        </Link>
+        {context.active ? (
+          <Link to="/dashboard" className="Navbar__Desktop--Item">
+            Dashboard
+          </Link>
+        ) : (
+          context.connectorName !== 'MetaMask' && (
+            <button onClick={() => context.setConnector('MetaMask')}>
+              Activate MetaMask
+            </button>
+          )
+        )}
+        {!context.active ? (
+          <p className="Rinkeby Navbar__Desktop--Item">Not connected</p>
+        ) : context.networkId === 4 ? (
+          <p className="Rinkeby Navbar__Desktop--Item">Rinkeby</p>
+        ) : (
+          <p className="Mainnet Navbar__Desktop--Item">Main</p>
+        )}
+      </div>
+      <HamburgerNav authenticated={context.active} />
+    </div>
+  );
+}
 
 export default HeaderLinks;
