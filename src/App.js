42e73b4d99afa829c0fb5c6394d0118301075512
dekan <dekanbro@gmail.com>
2019-02-26 14:46:18 -0700

    clean up dashboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
